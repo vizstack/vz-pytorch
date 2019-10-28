d147269dfe67e1349a24d63caa6b004036631ba2
Ryan Holmdahl <ryholmdahl@gmail.com>
2019-10-27 19:30:00 -0700

    Producing small RNN graph

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple, Any, Union, Dict
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models as tvmodels
 
 import vizstack as vz
@@ -18,14 +19,24 @@ class ComputationGraphNode:
         self.parent = None
         self.ancestors = []
         self.alignments = []
+        self.temporal_groups = [[]]
+        self.temporal_idx = -1
 
     def set_ancestors(self, ancestors: List["ComputationGraphNode"]):
         if len(ancestors) > 0:
             self.parent = ancestors[-1]
             self.ancestors = list(ancestors)
+            self.parent.temporal_groups[-1].append(self)
+            self.temporal_idx = len(self.parent.temporal_groups) - 1
 
     def has_ancestor(self, ancestor: "ComputationGraphNode"):
         return ancestor in self.ancestors
+
+    def create_temporal(self):
+        self.temporal_groups.append([])
+        last = self.temporal_groups[-2].pop()
+        last.temporal_idx += 1
+        self.temporal_groups[-1].append(last)
 
     def __view__(self):
         if self._variant == 'module':
@@ -38,8 +49,10 @@ class ComputationGraphNode:
 
 class ComputationGraph:
     def __init__(self):
-        self._stack: List[ComputationGraphNode] = []
-        self._nodes: List[ComputationGraphNode] = []
+        # TODO: make this invisible
+        self._root: ComputationGraphNode = ComputationGraphNode('root')
+        self._stack: List[ComputationGraphNode] = [self._root]
+        self._nodes: List[ComputationGraphNode] = [self._root]
         self._edges = []
         self._terminals = []
 
@@ -60,14 +73,20 @@ class ComputationGraph:
         d = vz.Dag()
         node_to_id = {}
         for i, n in enumerate(self._nodes):
-            d.node(str(i))
+            d.node(str(i), flow_direction='south' if len(n.temporal_groups) == 1 else 'east')
             d.item(n, str(i))
             node_to_id[n] = str(i)
+            if len(n.temporal_groups) > 1:
+                for t in range(len(n.temporal_groups)):
+                    d.node(f"{i}_{t}", parent=str(i), is_visible=True, flow_direction='south')
+                    d.item(None, f"{i}_{t}")
         for n in self._nodes:
             if n.parent:
-                d.node(node_to_id[n], parent=node_to_id[n.parent])
+                p = f"{node_to_id[n.parent]}_{n.temporal_idx}" if len(n.parent.temporal_groups) > 1 else node_to_id[n.parent]
+                d.node(node_to_id[n], parent=p)
             for other in n.alignments:
                 d.node(node_to_id[n], align_with={'axis': 'x', 'justify': 'north', 'nodes': [node_to_id[other]]})
+            print(node_to_id[n], len(n.temporal_groups))
         for edge in self._edges:
             d.port(node_to_id[edge[0]], edge[1], side='south' if edge[1].startswith('o') else 'north', order=int(edge[1][1:]))
             d.port(node_to_id[edge[2]], edge[3], side='south' if edge[3].startswith('o') else 'north', order=int(edge[3][1:]))
@@ -167,13 +186,15 @@ class Tracker:
 
         # for each param, check if it has a creator and add edge/alignment, then mark this as its creator
         if isinstance(m, nn.Module):
-            if not hasattr(m, '_cg_nodes'):
-                setattr(m, '_cg_nodes', [])
-            for _node in getattr(m, '_cg_nodes'):
-                if _node.parent == node.parent:
+            # if not hasattr(m, '_cg_nodes'):
+            #     setattr(m, '_cg_nodes', [])
+            for _node in node.parent.temporal_groups[-1]:
+                if node != _node and _node._payload == node._payload:
+                    # trigger a new temporal creation
+                    node.parent.create_temporal()
                     node.alignments.append(_node)
                     # create a container which wraps everything
-            getattr(m, '_cg_nodes').append(node)
+            # getattr(m, '_cg_nodes').append(node)
 
         # TODO: create data node for output if this is a terminal
 
@@ -205,11 +226,15 @@ class Model(nn.Module):
 
     def forward(self, x):
         x = self.l1(x)
-        x = self.relu(x)
-        x = self.s(x)
+        x = F.relu(x)
         x = self.l1(x)
+        x = F.relu(x)
+        x = self.l1(x)
+        x = F.relu(x)
         return x
 
+# Each parent maintains its own temporal steps
+# Whenever a new copy of anode is created within a given parent, wrap everything up to the last temporal boundary in a new temporal container
 
 def main():
     model = Model()
@@ -223,7 +248,7 @@ def main():
     graph = tracker.stop()
     with connect():
         get_logger('main').info(graph)
-    print(vz.assemble(graph))
+    print(str(vz.assemble(graph)).replace('None', 'null').replace('False', 'false').replace('True', 'true'))
 
 
 if __name__ == '__main__':
